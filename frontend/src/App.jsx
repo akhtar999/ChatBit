50c11ef675dec8a177d174d1897aa3a90d10fd83
Akhtar <iaktr999@gmail.com>
2023-11-02 00:18:59 +0530

    routing

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import ChatPage from "./pages/ChatPage";
+
+const App = () => {
+  return (
+    <div className="app">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/chats" element={<ChatPage />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
